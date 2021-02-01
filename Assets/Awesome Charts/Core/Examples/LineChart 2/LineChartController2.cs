c8f8819dca0dc41805934dd55560f8e2ff406f7a
tetoravr <tetosants@tetoravr.com>
2021-02-01 09:05:22 +0900

    チャート追加

---
+++ Assets/Awesome Charts/Core/Examples/LineChart 2/LineChartController2.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace AwesomeCharts {
+    public class LineChartController2 : MonoBehaviour {
+
+        public LineChart chart;
+
+        private void Start() {
+            ConfigChart();
+            //AddChartData();
+        }
+
+        private void ConfigChart() {
+
+            chart.Config.ValueIndicatorSize = 17;
+
+            chart.XAxis.DashedLine = true;
+            chart.XAxis.LineThickness = 1;
+            chart.XAxis.LabelColor = Color.white;
+            chart.XAxis.LabelSize = 18;
+
+            chart.YAxis.DashedLine = true;
+            chart.YAxis.LineThickness = 1;
+            chart.YAxis.LabelColor = Color.white;
+            chart.YAxis.LabelSize = 16;
+        }
+
+        public void AddChartData(float suds) {
+
+            chart.GetChartData().DataSets[0].AddEntry(new LineEntry(Time.realtimeSinceStartup, suds));
+            chart.SetDirty();
+        }
+    }
+}
