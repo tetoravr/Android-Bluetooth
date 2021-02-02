918fdb37613ebc3380564dd1c06035f988c5ebb3
tetoravr <tetosants@tetoravr.com>
2021-02-02 10:52:51 +0900

    Video　Script

---
+++ Assets/Scripts/VideoPlayerController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoPlayerController : MonoBehaviour
+{
+    /// <summary>
+    /// 制御するVideo Playerのリスト
+    /// </summary>
+    [SerializeField]
+    private List<VideoPlayer> playerList;
+    /// <summary>
+    /// 再生
+    /// </summary>
+    public void Play()
+    {
+        foreach (VideoPlayer player in playerList)
+        {
+            if (!player.isPlaying)
+            {
+                playerList[1].Stop();
+                playerList[0].Play();
+            }
+        }
+    }
+
+    public void changePlay()
+    {
+        playerList[0].Stop();
+        playerList[1].Play();
+            
+    }
+    
+    /// <summary>
+    /// 一時停止
+    /// </summary>
+    public void Pause()
+    {
+        foreach (VideoPlayer player in playerList)
+        {
+            if (player.isPlaying)
+            {
+                player.Pause();
+            }
+        }
+    }
+    /// <summary>
+    /// 停止
+    /// </summary>
+    public void Stop()
+    {
+        foreach (VideoPlayer player in playerList)
+        {
+            player.Stop();
+        }
+    }
+
+}
